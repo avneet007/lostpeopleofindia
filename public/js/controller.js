4d7064e9a6ae74369b0c55fd5930879a8ef54d61
avneet007 <avneetkumar007@gmail.com>
2017-02-16 00:07:56 +0530

    controller added & ng submit call on login

--- public/js/controller.js
+++ public/js/controller.js
@@ -2,4 +2,13 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
+
+     $scope.submit = function() {
+
+     	console.log($scope.useremail+$scope.userepass);
+       /* if ($scope.text) {
+          $scope.list.push(this.text);
+          $scope.text = '';
+        }*/
+      };
 });
